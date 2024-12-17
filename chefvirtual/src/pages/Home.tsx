ec464d0cc0445968644b6036f5fda2385197220e
Mariana Dias <marianamkt6@gmail.com>
2024-12-17 15:51:04 -0300

    first commit

---
+++ chefvirtual/src/pages/Home.tsx
@@ -0,0 +1,16 @@
+
+
+
+function Home() {
+  
+    return (
+      <>
+        <h1 className="text-3xl font-bold underline text-red-500 ">
+        Home 
+      </h1>
+      </>
+    )
+  }
+  
+  export default Home
+  
