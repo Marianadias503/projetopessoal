ec561e5f63d18d36135a940cd40a3e5ee6f04301
Mariana Dias <marianamkt6@gmail.com>
2025-02-10 16:26:22 -0300

    página para vizualizar todas as receitas

--- chefvirtual/src/pages/AllRecipes.tsx
+++ chefvirtual/src/pages/AllRecipes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useFetchRecipes } from "../hooks/useFecthRecipes"; 
 import Logo from "../assets/logo.png"; 
+
 const AllRecipesPage: React.FC = () => {
 
   // Usando o hook para buscar todas as receitas
@@ -15,13 +16,12 @@ const AllRecipesPage: React.FC = () => {
 
   if (loading) {
     return (
-    //logo
-    <div className=" flex flex-col items-center justify-between min-h-screen p-0 bg-[#333333]">
-      <img className={"w-60 rounded-full"} src={Logo} alt="Logo da aplicação" />
-    <h1>Receita sendo carregada </h1>
-    </div>
-    
-)
+      // Logo e mensagem de carregamento 
+      <div className="flex flex-col items-center justify-center min-h-screen bg-[#333333]">
+        <img className="w-60 rounded-full" src={Logo} alt="Logo da aplicação" />
+        <h1 className="text-white mt-4"> Receita sendo carregada...</h1>
+      </div>
+    );
   }
 
   if (error) {
@@ -29,13 +29,18 @@ const AllRecipesPage: React.FC = () => {
   }
 
   return (
-    <div className=" grid-cols-3">
-      <h1>Todas as Receitas</h1>
-      <div className="grid grid-cols-3 gap-6">
+    <div className="flex flex-col items-center p-8">
+      {/* Título  */}
+      <h1 className="mb-10 text-center text-3xl">Todas as Receitas</h1>
+
+      {/* usei o display grid para exibir as receitas de forma responsiva  */}
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-12 w-full">
         {recipes.map((recipe) => (
-          <div key={recipe.id} className="recipe-card border p-4 rounded-md">
-            <h2>{recipe.title}</h2>
-            {/* Botão para redirecionar para a página da receita */}
+          <div key={recipe.id} className="border rounded-sm border-black w-full flex flex-col items-center justify-center p-4">
+            {/* título da receita  */}
+            <h2 className="text-center text-xl mb-4">{recipe.title}</h2>
+
+            {/* passando a função handleViewRecipe para navegar até a pa´gina de detalhes da receita  */}
             <button
               onClick={() => handleViewRecipe(recipe)} // Passando a receita inteira
               className="bg-orange-500 text-white py-2 px-4 rounded mt-4"
